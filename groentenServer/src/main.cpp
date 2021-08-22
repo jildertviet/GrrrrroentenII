1040f316e343ef6c533af85c61f19dfc53a9cc78
Jildert Viet <jivido@hotmail.com>
2021-08-22 11:11:48 +0200

    test

--- groentenServer/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "ofApp.h"
-#include "ofAppNoWindow.h"
-
-int main(){
-    ofAppNoWindow window;
-    ofSetupOpenGL(&window, 1, 1, OF_WINDOW);
-    return ofRunApp(std::make_shared<ofApp>());
-}
